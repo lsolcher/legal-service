5c1a0daf44b2876ec87cd0b65248ce4f315581c7
lucas <lucas.solcher@digitalservice.bund.de>
2024-11-14 14:50:33 +0100

    Add react icons

--- app/components/util/Error.tsx
+++ app/components/util/Error.tsx
@@ -2,8 +2,8 @@ import { FaExclamationCircle } from 'react-icons/fa';
 
 function Error({ title, children }) {
   return (
-    <div className="error">
-      <div className="icon">
+    <div className='error'>
+      <div className='icon'>
         <FaExclamationCircle />
       </div>
       <h2>{title}</h2>
--- app/components/util/Logo.tsx
+++ app/components/util/Logo.tsx
@@ -2,8 +2,8 @@ import { Link } from '@remix-run/react';
 
 function Logo() {
   return (
-    <h1 id="logo">
-      <Link to="/">Gesetze analysieren</Link>
+    <h1 id='logo'>
+      <Link to='/'>Gesetze analysieren</Link>
     </h1>
   );
 }
